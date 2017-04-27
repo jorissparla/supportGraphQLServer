a825e1b65ab6453c187586292c07f897016baad4
joris sparla <joris.sparla@gmail.com>
2017-04-27 22:15:36 +0200

    added https

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+const https = require('https');
+const fs = require('fs');
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import schema from './data/schema';
@@ -7,7 +9,7 @@ import cors from 'cors';
 
 const PORT = 4000;
 const server = express();
-server.use('*', cors({ origin: 'http://localhost:3000' }));
+server.use('*', cors());
 server.use(
   '/graphql',
   bodyParser.json(),
@@ -22,7 +24,15 @@ server.use(
     endpointURL: '/graphql'
   })
 );
-
+https
+  .createServer(
+    {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem')
+    },
+    server
+  )
+  .listen(55555);
 server.listen(PORT, () =>
   console.log(`GraphQL Server is now running on http://localhost:${PORT}`)
 );
